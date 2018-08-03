93b2ee45007b0de9e4dad1c798f33f5c7953d6ea
Julio C. Rocha <julio@rochsquadron.net>
2018-08-03 00:42:39 -0700

    Define IAVAudioPlayer interface.

--- Apple/Audio/AppleJcfPlayer.cs
+++ Apple/Audio/AppleJcfPlayer.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+
+using Jammit.Model;
 
 namespace Jammit.Audio
 {
@@ -8,25 +11,67 @@ namespace Jammit.Audio
   {
     #region private members
 
-
+    private JcfMedia media;
+    private IAVAudioPlayer player;
 
     #endregion // private members
+
+    public AppleJcfPlayer(JcfMedia media, IAVAudioPlayer player)
+    {
+      this.media = media;
+      this.player = player;
+    }
 
     #region IJcfPlayer members
 
     public void Play()
     {
+      if (PlaybackStatus.Playing == State)
+        return;
 
+      // Dispose any existing playback.
+      if (player != null)
+      {
+        player.Stop();
+        player.Dispose();
+      }
+
+      var track = media.InstrumentTracks[0];
+      var path = Path.Combine(media.Path, $"{track.Identifier}_jcfx");
+      var stream = File.OpenRead(path);
+
+      //TODO: Load stream into player.
+      //player = AVAudioPlayer.FromData(NSData.FromStream(stream), out err);
+      player.Volume = 0.50f;
+      //TODO: //player.FinishedPlaying += delegate
+      player.NumberOfLoops = 1;
+
+      Length = TimeSpan.FromSeconds(player.Duration);
+      player.Play();
+
+      State = PlaybackStatus.Playing;
     }
 
     public void Pause()
     {
+      if (PlaybackStatus.Paused == State)
+        return;
 
+      Stop();
     }
 
     public void Stop()
     {
+      if (PlaybackStatus.Stopped == State)
+        return;
 
+      if (player == null)
+      {
+        player.Stop();
+        player.Dispose();
+      }
+
+      State = PlaybackStatus.Stopped;
     }
 
     public uint GetVolume(Model.PlayableTrackInfo track)
@@ -41,7 +86,7 @@ namespace Jammit.Audio
 
     public TimeSpan Position { get; set; }
 
-    public TimeSpan Length { get; set; }
+    public TimeSpan Length { get; private set; }
 
     public PlaybackStatus State { get; private set; }
 
---
+++ Apple/Audio/IAVAudioPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jammit.Audio
+{
+  public interface IAVAudioPlayer
+  {
+    void Play();
+
+    void PlayAtTime(double time);
+
+    void Stop();
+
+    void Dispose();
+
+    double Duration { get; }
+
+    float Volume { get; set; }
+
+    int NumberOfLoops { get; set; }
+  }
+}
