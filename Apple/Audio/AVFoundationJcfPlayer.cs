1217639bc37188eeb87738e63b1fdd00355aed81
Julio C. Rocha <julio@rochsquadron.net>
2018-08-02 23:56:43 -0700

    Added Unjammit.Apple project.

---
+++ Apple/Audio/AVFoundationJcfPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jammit.Audio
+{
+  public class AVFoundationJcfPlayer : IJcfPlayer
+  {
+    #region IJcfPlayer members
+
+    public void Play()
+    {
+
+    }
+
+    public void Pause()
+    {
+
+    }
+
+    public void Stop()
+    {
+
+    }
+
+    public uint GetVolume(Model.PlayableTrackInfo track)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void SetVolume(Model.PlayableTrackInfo track, uint volume)
+    {
+      throw new NotImplementedException();
+    }
+
+    public TimeSpan Position { get; set; }
+
+    public TimeSpan Length { get; set; }
+
+    public PlaybackStatus State { get; private set; }
+
+    #endregion // IJcfPlayer members
+  }
+}
