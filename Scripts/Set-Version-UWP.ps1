60575a92c4012201f43214b4368627e85a3a484a
Julio C. Rocha <julio@rochsquadron.net>
2018-12-10 23:45:46 -0800

    Make UWP Set-Verison only demand Version argument.

--- Scripts/Set-Version-UWP.ps1
+++ Scripts/Set-Version-UWP.ps1
@@ -3,10 +3,8 @@ param(
 	[ValidateScript({Test-Path $_})]
 	[string] $AppxManifest,
 
-	[Parameter(Mandatory=$true)]
 	[string] $PackageIdentityName,
 
-	[Parameter(Mandatory=$true)]
 	[string] $PackageDisplayName,
 
 	[Parameter(Mandatory=$true)]
@@ -19,8 +17,14 @@ $xml = New-Object -TypeName System.Xml.XmlDocument
 $xml.PreserveWhitespace = $true
 $xml.Load($manifest)
 
-$xml.Package.Identity.Name = $PackageIdentityName
-$xml.Package.Properties.DisplayName = $PackageDisplayName
+if ($PackageIdentityName) {
+	$xml.Package.Identity.Name = $PackageIdentityName
+}
+if ($PackageDisplayName) {
+	$xml.Package.Properties.DisplayName = $PackageDisplayName
+	$xml.Package.Applications.Application.VisualElements.DisplayName = $PackageDisplayName
+}
+
 $xml.Package.Identity.Version = "$Version"
 
 $xml.Save($manifest)
