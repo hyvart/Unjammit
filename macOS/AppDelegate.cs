35f2405d6dea6ca42aeba08cc1e2700d402e4165
Julio C. Rocha <julio@rochsquadron.net>
2019-02-23 18:33:02 -0800

    [macOS] Remove hard-coded window title.

--- macOS/AppDelegate.cs
+++ macOS/AppDelegate.cs
@@ -16,7 +16,6 @@ namespace Jammit.macOS
 
       var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
       _window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
-      _window.Title = "Xamarin.Forms on Mac!";
       _window.TitleVisibility = NSWindowTitleVisibility.Hidden;
     }
 
