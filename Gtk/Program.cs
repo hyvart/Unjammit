7d49c7c3696acffb963f10a1149679f6b6b79b8f
Julio César Rocha <julio@rochsquadron.net>
2019-04-20 05:20:31 -0700

    Added LibVLCSharp.Gtk Forms dependency.

--- Gtk/Program.cs
+++ Gtk/Program.cs
@@ -12,6 +12,9 @@ namespace Jammit.Gtk
       global::Gtk.Application.Init();
       Xamarin.Forms.Forms.Init();
 
+      global::LibVLCSharp.Forms.Shared.LibVLCSharpFormsRenderer.Init();
+      global::LibVLCSharp.Shared.Core.Initialize();
+
       var dataDir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
       dataDir = System.IO.Path.Combine(dataDir, "Unjammit");
       // Create dataDir, if it doesnt' exist.
@@ -20,7 +23,7 @@ namespace Jammit.Gtk
 
       Jammit.Forms.App.DataDirectory = dataDir;
       Jammit.Forms.App.MediaLoader = new Model.FileSystemJcfLoader(dataDir);
-      Jammit.Forms.App.PlayerFactory = (media) => null;
+      Jammit.Forms.App.PlayerFactory = (media) => new Audio.VlcJcfPlayer(media);
 
       var app = new Jammit.Forms.App();
       var window = new FormsWindow();
