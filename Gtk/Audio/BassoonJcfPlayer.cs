4b65361779e9ced4ccb5bb2121a51657f46ae89e
Julio C. Rocha <julio@rochsquadron.net>
2019-04-20 19:04:02 -0700

    Added Bassoon JCF Player.

---
+++ Gtk/Audio/BassoonJcfPlayer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using Bassoon;
+using Jammit.Model;
+
+namespace Jammit.Audio
+{
+  public class BassoonJcfPlayer : IJcfPlayer
+  {
+    public BassoonJcfPlayer(Model.JcfMedia media)
+    {
+      using (BassoonEngine be = new BassoonEngine())
+      {
+        var sound = new Sound(media.Path);
+      }
+    }
+
+    #region IJcfPlayer
+
+    public TimeSpan Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public TimeSpan Length => throw new NotImplementedException();
+
+    public PlaybackStatus State => throw new NotImplementedException();
+
+    public event EventHandler PositionChanged;
+
+    public uint GetVolume(PlayableTrackInfo track)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Pause()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Play()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void SetVolume(PlayableTrackInfo track, uint volume)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion //IJcfPlayer
+  }
+}
---
+++ Gtk/Audio/SdlJcfPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Jammit.Model;
+
+namespace Jammit.Audio
+{
+  public class SdlJcfPlayer : IJcfPlayer
+  {
+    public SdlJcfPlayer(JcfMedia media)
+    {
+
+    }
+
+    #region IJcfPlayer
+
+    public TimeSpan Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public TimeSpan Length => throw new NotImplementedException();
+
+    public PlaybackStatus State => throw new NotImplementedException();
+
+    public event EventHandler PositionChanged;
+
+    public uint GetVolume(PlayableTrackInfo track)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Pause()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Play()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void SetVolume(PlayableTrackInfo track, uint volume)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion // IJcfPlayer
+  }
+}
