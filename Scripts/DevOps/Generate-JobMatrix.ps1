0cf63e51eed64a051eacf4f19fb2da1a458b0111
Julio C. Rocha <julio@rochsquadron.net>
2019-09-24 21:22:14 -0700

    Remove redundant LangVersion.
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Update
    
    Re-enabled iOS build.
    
    Use msbuild restore for iOS.
    
    iOS: Allow implicit build platform
    
    Update
    
    Update
    
    Update
    
    Update
    
    Implement Generate-JobMatrix.
    
    Define steps template: build-uwp.
    
    Corrected matrix ordering.

---
+++ Scripts/DevOps/Generate-JobMatrix.ps1
@@ -0,0 +1,34 @@
+<#
+	Generates a matrix as defined by the Azure Pipelines schema.
+	Outputs results in JSON format.
+#>
+param(
+	[Parameter(Mandatory=$true)]
+	[System.Collections.Specialized.OrderedDictionary] $Matrix,
+
+	[switch] $Compress
+)
+
+$total = 1;
+foreach ($k in $Matrix.Keys) {
+	$total = $total * $Matrix[$k].Count
+}
+
+$result = [ordered]@{}
+[string[]]$keys = $Matrix.Keys
+foreach($i in 0..($total-1)) {
+	$m = 1
+	$values = @()
+	foreach ($j in ($keys.Count-1)..0) {
+		$values = @($Matrix[ $keys[$j] ][ [Math]::Floor($i / $m) % $Matrix[$keys[$j]].Count ]) + $values
+		$m = $m * $Matrix[$keys[$j]].Count
+	}
+
+	$newKey = $values -join '|'
+	$result[$newKey] = [ordered]@{}
+	foreach($j in 0..($keys.Count-1)) {
+		$result[$newKey][$keys[$j]] = $values[$j]
+	}
+}
+
+return $result | ConvertTo-Json -Compress:$Compress
