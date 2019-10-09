412059882494068d2902270d311b312cd6006269
Julio C. Rocha <julio@rochsquadron.net>
2019-10-08 22:48:21 -0700

    Use plist-cil 2.0.0 for Set-Version.

--- Scripts/Set-Version-Apple.ps1
+++ Scripts/Set-Version-Apple.ps1
@@ -28,15 +28,15 @@ $plist = Get-ChildItem -Path $InfoPlist
 
 if ($UsePlistCil) {
 	# Install plist-cil
-	if (! (Test-Path "$DownloadDir\plist-cil.1.50.0\lib\netstandard2.0\plist-cil.dll")) {
+	if (! (Test-Path "$DownloadDir\plist-cil.2.0.0\lib\netstandard2.0\plist-cil.dll")) {
 		if (! (Test-Path $DownloadDir)) {
 			New-Item -ItemType Directory $DownloadDir
 		}
 
-		nuget install plist-cil -Version 1.50.0 -OutputDirectory $DownloadDir
+		nuget install plist-cil -Version 2.0.0 -OutputDirectory $DownloadDir
 	}
 
-	Add-Type -Path "$DownloadDir\plist-cil.1.50.0\lib\netstandard2.0\plist-cil.dll"
+	Add-Type -Path "$DownloadDir\plist-cil.2.0.0\lib\netstandard2.0\plist-cil.dll"
 
 	$dict = [Claunia.PropertyList.PropertyListParser]::Parse($plist)
 
