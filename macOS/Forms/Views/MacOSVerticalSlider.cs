5d3bbd0c9c553d9945b81a3b229d3481063ff438
Julio C. Rocha <julio@rochsquadron.net>
2019-11-19 01:01:16 -0800

    Define VerticalSlider.

---
+++ macOS/Forms/Views/MacOSVerticalSlider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using AppKit;
+
+namespace Jammit.Forms.Views
+{
+  /// <summary>
+  /// See https://github.com/xamarin/Xamarin.Forms/blob/release-4.3.0-sr2/Xamarin.Forms.Platform.MacOS/Controls/FormsNSSlider.cs
+  /// See https://github.com/xamarin/xamarin-macios/blob/xamarin-mac-5.2.1.11/src/AppKit/NSSlider.cs
+  /// </summary>
+  public class MacOSVerticalSlider : NSSlider
+  {
+    public MacOSVerticalSlider()
+    {
+    }
+  }
+}
