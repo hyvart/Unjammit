00f34d2b148842d7395072bb7306cec4167926b1
Julio C. Rocha <julio@rochsquadron.net>
2019-11-20 11:47:22 -0800

    Enabled GtkVerticalSliderRenderer

--- Gtk/Forms/Renderers/GtkVerticalSliderRenderer.cs
+++ Gtk/Forms/Renderers/GtkVerticalSliderRenderer.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 
 using Xamarin.Forms;
-//using Xamarin.Forms.Platform.GTK;
+using Xamarin.Forms.Platform.GTK;
+using Xamarin.Forms.Platform.GTK.Renderers;
 
-//[assembly: ExportRenderer(typeof(Jammit.Forms.Views.VerticalSlider), typeof(Jammit.Forms.Renderers.GtkVerticalSliderRenderer))]
+[assembly: ExportRenderer(typeof(Jammit.Forms.Views.VerticalSlider), typeof(Jammit.Forms.Renderers.GtkVerticalSliderRenderer))]
 namespace Jammit.Forms.Renderers
 {
-  public class GtkVerticalSliderRenderer
+  public class GtkVerticalSliderRenderer : SliderRenderer
   {
-    public GtkVerticalSliderRenderer()
+    #region ViewRenderer overrides
+
+    protected override void OnElementChanged(ElementChangedEventArgs<Slider> e)
     {
+      base.OnElementChanged(e);
+
+      if (Control != null)
+      {
+
+      }
     }
+
+    #endregion ViewRenderer overrides
   }
 }
