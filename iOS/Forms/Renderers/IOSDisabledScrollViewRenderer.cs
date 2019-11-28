398a3c5e7f7069e722eedc2b8207112899f089d3
Julio C. Rocha <julio@rochsquadron.net>
2019-11-28 04:17:04 -0800

    Implement IOSDisabledScrollViewRenderer.

---
+++ iOS/Forms/Renderers/IOSDisabledScrollViewRenderer.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Jammit.Forms.Views.DisabledScrollView), typeof(Jammit.Forms.Renderers.IOSDisabledScrollViewRenderer))]
+namespace Jammit.Forms.Renderers
+{
+  public class IOSDisabledScrollViewRenderer : ScrollViewRenderer
+  {
+    protected override void OnElementChanged(VisualElementChangedEventArgs e)
+    {
+      base.OnElementChanged(e);
+
+      ScrollEnabled = false;
+    }
+  }
+}
