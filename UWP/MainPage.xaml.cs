b60760104710eeb44738ae813c8f7cf4bcdc7554
Julio C. Rocha <julio@rochsquadron.net>
2019-12-08 06:21:57 -0800

    Remove comment.

--- UWP/MainPage.xaml.cs
+++ UWP/MainPage.xaml.cs
@@ -23,7 +23,6 @@ namespace Jammit.UWP
 
       Jammit.Forms.App.DataDirectory = Xamarin.Essentials.FileSystem.AppDataDirectory;
       Jammit.Forms.App.PlayerFactory = async (media) => await Audio.FFmpegJcfPlayer.CreateAsync(media);
-      //Jammit.Forms.App.PlayerFactory = async (media) => { return new Audio.FFmpegJcfPlayer(media); };
       Jammit.Forms.App.MediaLoader = new Model.FileSystemJcfLoader(Xamarin.Essentials.FileSystem.AppDataDirectory);
 
       LoadApplication(
