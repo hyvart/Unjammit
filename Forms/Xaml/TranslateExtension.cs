1b259a8c30d37972d2eaeeadf5dbf71ad962543a
Julio C. Rocha <julio@rochsquadron.net>
2020-08-10 23:41:57 -0700

    Add TranslateExtension

---
+++ Forms/Xaml/TranslateExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Jammit.Forms.Xaml
+{
+  [ContentProperty("Text")]
+  public class TranslateExtension : IMarkupExtension<BindingBase>
+  {
+    public string Text { get; set; }
+
+    #region IMarkupExtension
+
+    public BindingBase ProvideValue(IServiceProvider serviceProvider)
+    {
+      return ProvideValue(serviceProvider);
+    }
+
+    object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+    {
+      var binding = new Binding
+      {
+        Mode = BindingMode.OneWay,
+        Path = $"[{Text}]",
+        Source = LocalizationResourceManager.Instance
+      };
+
+      return binding;
+    }
+
+    #endregion IMarkupExtension
+  }
+}
