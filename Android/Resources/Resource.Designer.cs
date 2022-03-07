0fe2402caead913a682701e83f2160c37133e703
Julio C. Rocha <julio@rochsquadron.net>
2022-03-07 00:44:31 -0800

    Update Android RC designer

--- Android/Resources/Resource.Designer.cs
+++ Android/Resources/Resource.Designer.cs
@@ -14,7 +14,7 @@ namespace Jammit
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.1.99.62")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.0.155")]
 	public partial class Resource
 	{
 		
