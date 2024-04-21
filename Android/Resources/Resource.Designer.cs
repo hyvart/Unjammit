09d3e30b4207818544cf8e8b6cab5b44bef080e0
Julio C. Rocha <dev@rocha.red>
2024-04-20 23:58:01 -0700

    Update Android generated code

--- Android/Resources/Resource.Designer.cs
+++ Android/Resources/Resource.Designer.cs
@@ -15,7 +15,7 @@ namespace Jammit
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.1.111")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.2.120")]
 	public partial class Resource
 	{
 		
