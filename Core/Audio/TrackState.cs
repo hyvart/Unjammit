2fb3c8f0d3f9d98e05159e018750b19792a9298e
Julio C. Rocha <dev@rocha.red>
2024-08-11 03:46:57 -0700

    Define TrackState
    Describes the current state of a given audio track with two members:
    - Intended volume
    - Effective audio status\
      Whether sound is on and whether this is triggered manually or as a consequence of either soloing other tracks or countdown threshold reached

---
+++ Core/Audio/TrackState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Jammit.Audio
+{
+  public class TrackState
+  {
+    public enum AudioStatus
+    {
+      On,
+      Muted,      // Black-listed off
+      Solo,       // White-listed on
+      Excluded,   // Muted by exclusion of Solo white list
+      AutoMuted   // i.e. click track countdown
+    }
+
+    public AudioStatus Status = AudioStatus.On;
+
+    public uint Volume = 100;
+  }
+}
