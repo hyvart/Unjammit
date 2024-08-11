d487c7652b9093162efaafcb59e793b3e7c23df7
Julio C. Rocha <dev@rocha.red>
2024-08-11 03:50:03 -0700

    Define IJcfPlayer.{GetAudioStatus,Mute,Unmute}
    Helper methods to expose and set the effective sound state for the track.

--- Core/Audio/IJcfPlayer.cs
+++ Core/Audio/IJcfPlayer.cs
@@ -10,9 +10,15 @@ namespace Jammit.Audio
 
     void Stop();
 
+    TrackState.AudioStatus GetAudioStatus(Model.PlayableTrackInfo track);
+
     uint GetVolume(Model.PlayableTrackInfo track);
 
     void SetVolume(Model.PlayableTrackInfo track, uint volume);
+
+    void Mute(Model.PlayableTrackInfo track);
+
+    void Unmute(Model.PlayableTrackInfo track);
 
     uint TotalBeats { get; }
 
